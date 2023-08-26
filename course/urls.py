6cf7aa51a80a9d6c125fc43f423421632b8cf177
mohoab <138564219+mohoab@users.noreply.github.com>
2023-08-26 22:14:53 +0330

    rrt

--- course/urls.py
+++ course/urls.py
@@ -4,6 +4,8 @@ app_name = 'course'
 urlpatterns = [
     path('' , views.course , name = 'course' ) , 
     path('category/<str:cat>' , views.course , name = 'course_cat' ) , 
+    path('trainer/<str:trainer>' , views.course , name = 'course_trainer' ) , 
+    path('category/<str:cname>' , views.course , name = 'course_cname' ) , 
 ]
 
 
--- course/views.py
+++ course/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from .models import course as cr
 from course.models import category as cg
 
-def course(request,cat=None):
+def course(request,cat=None,trainer=None,cname=None):
     c = cg.objects.all()
     if cat :
        courses = cr.objects.filter(category__title=cat ) 
+    elif cname :
+       courses = cr.objects.filter(category__title=cname ) 
+    elif trainer :
+        courses = cr.objects.filter(trainer__info__username=trainer)
     else:
         courses = cr.objects.filter(status=True)
     
