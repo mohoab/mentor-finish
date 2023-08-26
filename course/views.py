71bd56a46953cea377cb2b6c6a0c304eb0fc71bb
mohoab <138564219+mohoab@users.noreply.github.com>
2023-08-26 19:44:59 +0330

    ok

--- course/views.py
+++ course/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import course as cr
+from course.models import category as cg
 
 def course(request):
     courses = cr.objects.filter(status=True)
+    categorys = cg.objects.all()
     context = {
-        'courses': courses
+        'courses': courses,
+        'category' : categorys,
     }
     return render( request , 'course\courses.html' , context=context )
--- root/views.py
+++ root/views.py
@@ -1,26 +1,51 @@
 from django.shortcuts import render
 from course.models import course as cr
 from course.models import trainer as tr
+from course.models import category as cg
+from django.contrib.auth.models import User as us
 from .models import service as service
+
 t='trainers.html'
 a='about.html'
 def home(request):
+    user_count = us.objects.all().count()
+    course_count = cr.objects.filter(status=True).count()
+    trainer_count = tr.objects.filter(status=True).count()
+    service_count = service.objects.filter(status=True).count()
     last_three_courses= cr.objects.filter(status=True)[:3]
     last_three_trainer= tr.objects.filter(status=True)[:3]
     sr = service.objects.filter(status=True)[:3]
+    categorys = cg.objects.all()
 
 
     context ={
         'trainers' : last_three_trainer ,
         'courses' : last_three_courses ,
         'services' : sr , 
+        'category': categorys ,
+        'uc' : user_count , 
+        'cc' : course_count , 
+        'tc' : trainer_count , 
+        'sc' : service_count ,
     }
     return render( request , 'root\index.html' , context=context )
 def trainer(request):
-    return render( request , f'root\{t}' )
+    categorys = cg.objects.all()
+    context = {
+        'category' : categorys
+    }
+    return render( request , f'root\{t}' ,context=context)
 
 def contact(request):
-    return render( request , 'root\contact.html' )
+    categorys = cg.objects.all()
+    context = {
+        'category' : categorys ,
+    }
+    return render( request , 'root\contact.html' ,context=context )
 
 def about(request):
-    return render( request , f'root\{a}' )
+    categorys = cg.objects.all()
+    context = {
+        'category' : categorys
+    }
+    return render( request , f'root\{a}' , context=context)
